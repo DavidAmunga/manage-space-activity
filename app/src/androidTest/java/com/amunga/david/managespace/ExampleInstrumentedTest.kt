0c64f5e9b041d30d11ad31112c75f2c0848c33a3
David Amunga <david.onyimbo@gmail.com>
2022-02-03 22:32:50 +0300

    Initial Commit

---
+++ app/src/androidTest/java/com/amunga/david/managespace/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.amunga.david.managespace
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.amunga.david.managespace", appContext.packageName)
+    }
+}
